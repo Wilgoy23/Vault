316ebcb713ca66b86c305b96b2eb4cd9194dc020
Wilgoy23 <willgoyens23@gmail.com>
2026-05-05 22:35:22 -0400

    feat: update release workflow permissions and add additional build targets

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import Overlay from "./Overlay";
-import "./app.css";
+import "./App.css";
 import { getCurrentWindow } from "@tauri-apps/api/window";
 
 const isOverlay = getCurrentWindow().label === "overlay";
